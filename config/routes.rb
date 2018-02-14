2a28a460917b3da0710766386df3f03ae4b3768f
pacoturcotte <pascal.turcotte@uqar.ca>
2018-02-13 23:13:11 -0500

    Création du contrôleur...

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'clients/new'
+
+  get 'clients/create'
+
+  get 'clients/update'
+
+  get 'clients/edit'
+
+  get 'clients/destroy'
+
+  get 'clients/index'
+
+  get 'clients/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
